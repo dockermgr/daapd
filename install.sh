d87b8f169e8420ca13d4f5ef484b70923b07531f
casjay <casjay@yahoo.com>
2020-09-09 16:11:40 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-mkdir -p /srv/docker/daapd/{etc,music} && chmod -Rf 777 /srv/docker/daapd
+APPNAME="daapd"
+DATADIR="/srv/docker/$APPNAME"
 
+mkdir -p "$DATADIR"/{config,music} && chmod -Rf 777 "$DATADIR"
+
+if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
+docker pull linuxserver/daapd && docker restart $APPNAME
+else
 docker run -d \
---name=daapd \
---hostname daapd \
+--name="$APPNAME" \
+--hostname "$APPNAME" \
 --net=host \
--v /srv/docker/daapd/etc:/config \
--v /srv/docker/music:/music \
-registry.casjay.in/latest/daapd:latest
+-e TZ=America/New_York \
+-v $DATADIR/config:/config \
+-v $DATADIR/music:/music \
+linuxserver/daapd
