c0a3187cfcad9cc73bfbd54d617d6ecbab06260c
Jason <jason@casjay.net>
2021-01-29 23:27:57 -0500

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 APPNAME="daapd"
-DATADIR="/srv/docker/$APPNAME"
 
 mkdir -p "$DATADIR"/{config,music} && chmod -Rf 777 "$DATADIR"
 
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
-docker pull linuxserver/daapd && docker restart $APPNAME
+  docker pull linuxserver/daapd && docker restart "$APPNAME"
 else
-docker run -d \
---name="$APPNAME" \
---hostname "$APPNAME" \
---net=host \
--e TZ=America/New_York \
--v $DATADIR/config:/config \
--v $DATADIR/music:/music \
-linuxserver/daapd
+  docker run -d \
+    --name="$APPNAME" \
+    --hostname "$APPNAME" \
+    --net=host \
+    -e TZ=America/New_York \
+    -v "$DATADIR/config":/config \
+    -v "$DATADIR/music":/music \
+    linuxserver/daapd
+fi
