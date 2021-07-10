9d3ee499528fcd6ecd02cc69f08ca73b6a276b9b
Jason <jason@casjay.net>
2021-07-10 15:54:08 -0400

    🐛 FIXED: install.sh 🐛

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
 APPNAME="daapd"
+DOCKER_HUB_URL="linuxserver/daapd"
 
-mkdir -p "$DATADIR"/{config,music} && chmod -Rf 777 "$DATADIR"
+sudo mkdir -p "$DATADIR"/{config,music}
+sudo chmod -Rf 777 "$DATADIR"
 
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
-  docker pull linuxserver/daapd && docker restart "$APPNAME"
+  sudo docker pull "$DOCKER_HUB_URL"
+  sudo docker restart "$APPNAME"
 else
-  docker run -d \
+  sudo docker run -d \
     --name="$APPNAME" \
     --hostname "$APPNAME" \
     --net=host \
     -e TZ=America/New_York \
     -v "$DATADIR/config":/config \
     -v "$DATADIR/music":/music \
-    linuxserver/daapd
+    "$DOCKER_HUB_URL"
 fi
